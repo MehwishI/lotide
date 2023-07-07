6bdc567ab0e21fc347b12412fc4bf20b9c4eccd6
Mehwish Iqbal <mehwishned21@gmail.com>
2023-07-07 09:29:24 -0500

    Object Shorthand

--- index.js
+++ index.js
@@ -17,20 +17,20 @@ const letterPositions = require('./letterPositions');
 const map = require('./map');
 
 module.exports = {
-  head: head,
-  tail: tail,
-  middle: middle,
-  assertEqual: assertEqual,
-  assertArraysEqual: assertArraysEqual,
-  eqArrays: eqArrays,
-  assertObjectsEqual: assertObjectsEqual,
-  eqObjects: eqObjects,
-  countLetters: countLetters,
-  countOnly: countOnly,
-  findKey: findKey,
-  findKeyByValue: findKeyByValue,
-  flatten: flatten,
-  letterPositions: letterPositions,
-  map: map
+  head,
+  tail,
+  middle,
+  assertEqual,
+  assertArraysEqual,
+  eqArrays,
+  assertObjectsEqual,
+  eqObjects,
+  countLetters,
+  countOnly,
+  findKey,
+  findKeyByValue,
+  flatten,
+  letterPositions,
+  map
 
 };
