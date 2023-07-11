cc6a2b71829e03bee065c57e72bb2bf8dd9ee1d1
Mehwish Iqbal <mehwishned21@gmail.com>
2023-07-11 13:55:43 -0500

    imported eqObjects function and used in assertObjectsEqual function

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,27 +1,5 @@
-const eqArrays = require("./eqArrays");
+const eqObjects = require("./eqObjects");
 
-const eqObjects = function (object1, object2) {
-  if (Object.keys(object1).length === Object.keys(object2).length) {
-    for (let key of Object.keys(object1)) {
-      if (
-        Object.keys(object2).includes(key) &&
-        Array.isArray(object1[key]) &&
-        Array.isArray(object2[key])
-      ) {
-        if (!eqArrays(object1[key], object2[key])) {
-          return false;
-        }
-      } else {
-        if (
-          !(Object.keys(object2).includes(key) && object1[key] === object2[key])
-        ) {
-          return false;
-        }
-      }
-    }
-    return true;
-  }
-};
 const assertObjectsEqual = function (object1, object2) {
   const inspect = require("util").inspect;
 
